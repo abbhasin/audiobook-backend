571f41ea8cf9584d5618540a9021bc827b839b13
Akhil Bhasin <akhilbhasin1991@gmail.com>
2024-04-01 08:56:14 +0530

    date fix

--- src/main/java/com/enigma/audiobook/backend/utils/MongoDateConverter.java
+++ src/main/java/com/enigma/audiobook/backend/utils/MongoDateConverter.java
@@ -5,25 +5,26 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.utils.DateUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Slf4j
 public class MongoDateConverter extends JsonDeserializer<Date> {
+    private static final String[] acceptedFormats = {"yyyy-MM-dd'T'HH:mm:ss.SSSX", "yyyy-MM-dd'T'HH:mm:ssX", "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"};
 
     @Override
     public Date deserialize(JsonParser jp, DeserializationContext ctxt) {
-        // formatter is not thread safe, make local: https://medium.com/@daveford/numberformatexception-multiple-points-when-parsing-date-650baa6829b6
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
         try {
             JsonNode node = jp.readValueAsTree();
             if (node.isTextual()) {
-                return formatter.parse(node.asText());
+                return DateUtils.parseDate(node.asText(), acceptedFormats);
             }
 
-            return  formatter.parse(node.get("$date").asText());
+            return DateUtils.parseDate(node.get("$date").asText(), acceptedFormats);
         } catch (Exception e) {
+            log.error("unable to parse date", e);
             throw new RuntimeException(e);
         }
     }
